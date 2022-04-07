390d6169d522b62c443099f8bd2ffc2aaab6852d
Annaavh <annaavetisyan175@gmail.com>
2022-04-07 14:04:44 +0400

    task

---
+++ task/src/Page1.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import userApi from "./api/api"
+
+function Page1() {
+    useEffect(()=>{
+         foo()
+      
+    },[])
+    async function foo(){
+        userApi.post('/init', 
+        {
+            firstName: 'Anna',
+            lastName: 'Avetisyan',
+            birthDay:"23.08.2000",
+            gender:"female"
+
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+    }
+    
+  return (
+    <div>
+        <table className="table mt-2">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+         
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Page1
---
+++ task/src/api/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+
+    export default axios.create({
+        baseURL:"http://intern-2022.arpify.com"
+    })
